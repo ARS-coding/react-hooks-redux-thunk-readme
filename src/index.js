bbfba38fcee52540da87ba561564178455489b56
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-16 19:06:33 +0300

    Compose our thunk implemented redux store with the redux development tools.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import App from "./App";
 import rootReducer from "./reducer";
 
-const thunk = applyMiddleware(thunkMiddleware);
+const composedEnhacer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-const store = createStore( rootReducer, thunk );
+const store = createStore( rootReducer, composedEnhacer );
 
 ReactDOM.render(
   <Provider store={store}>
